47b67a698d1d24f068eef622a79931846e44b5c2
Štefan Oravec <stefan.oravec@ceosdata.com>
2023-01-30 12:40:03 +0100

    fix multi dimensional gauges

--- test/metrics.test.js
+++ test/metrics.test.js
@@ -5,98 +5,110 @@ const metrics = require('../lib/metrics');
 const trueThing = true;
 
 describe('formatHeader', () => {
-  it('returns correctly formatted metric header', () => {
-    const expected = 
-      '# HELP Foo helptext\n' +
-      '# TYPE Foo counter';
-    const actual = metrics.formatHeader({ name: 'Foo', type: 'counter', help: 'helptext'});
-    assert.equal(actual, expected);
-  });
+    it('returns correctly formatted metric header', () => {
+        const expected =
+            '# HELP Foo helptext\n' +
+            '# TYPE Foo counter';
+        const actual = metrics.formatHeader({name: 'Foo', type: 'counter', help: 'helptext'});
+        assert.equal(actual, expected);
+    });
 });
 
 describe('getValue', () => {
-  it('returns the value of the according path', () => {
-    assert.equal(2, metrics.getValue({a:{b:2}}, ['a', 'b']));
-  });
+    it('returns the value of the according path', () => {
+        assert.equal(2, metrics.getValue({a: {b: 2}}, ['a', 'b']));
+    });
 
-  it('returns undefined for invalid paths', () => {
-    assert.equal(undefined, metrics.getValue({a:{b:2}}, ['a', 'c']));
-  });
+    it('returns undefined for invalid paths', () => {
+        assert.equal(undefined, metrics.getValue({a: {b: 2}}, ['a', 'c']));
+    });
 });
 
 describe('makeGauge', () => {
-  it('returns a gauge metric object', () => {
-    const metric = { name: 'metric', type: 'gauge', path: ['a', 'b'], help: 'help'};
-    const actual = metrics.makeGauge(metric.name, metric.path, metric.help);
-    assert.equal(actual.name, metric.name);
-    assert.equal(actual.type, metric.type);
-    assert.equal(actual.path, metric.path);
-    assert.equal(actual.help, metric.help);
-  });
+    it('returns a gauge metric object', () => {
+        const metric = {name: 'metric', type: 'gauge', path: ['a', 'b'], help: 'help'};
+        const actual = metrics.makeOneDimensionalGauge(metric.name, metric.path, metric.help);
+        assert.equal(actual.name, metric.name);
+        assert.equal(actual.type, metric.type);
+        assert.equal(actual.path, metric.path);
+        assert.equal(actual.help, metric.help);
+    });
 
-  it('format function returns correctly formated metric', () => {
-    const metric = metrics.makeGauge('metric', ['a', 'b'], '');
-    const stats = {a: {b: 42}};
-    const actual = metric.format(stats);
-    assert.equal(actual, metrics.formatHeader(metric) + '\nmetric 42');
-  });
+    it('format function returns correctly formated metric', () => {
+        const metric = metrics.makeOneDimensionalGauge('metric', ['a', 'b'], '');
+        const stats = {a: {b: 42}};
+        const actual = metric.format(stats);
+        assert.equal(actual, metrics.formatHeader(metric) + '\nmetric 42');
+    });
 });
 
 describe('makeHistogram', () => {
-  it('returns a histogram metric object', () => {
-    const metric = { name: 'metric', type: 'histogram', path: ['a', 'b'], help: 'help', cuts: [0, 1]};
-    const actual = metrics.makeHistogram(metric.name, metric.path, metric.cuts, metric.help);
-    assert.equal(actual.name, metric.name);
-    assert.equal(actual.type, metric.type);
-    assert.equal(actual.path, metric.path);
-    assert.equal(actual.help, metric.help);
-    assert.equal(actual.cuts, metric.cuts);
-  });
+    it('returns a histogram metric object', () => {
+        const metric = {name: 'metric', type: 'histogram', path: ['a', 'b'], help: 'help', cuts: [0, 1]};
+        const actual = metrics.makeHistogram(metric.name, metric.path, metric.cuts, metric.help);
+        assert.equal(actual.name, metric.name);
+        assert.equal(actual.type, metric.type);
+        assert.equal(actual.path, metric.path);
+        assert.equal(actual.help, metric.help);
+        assert.equal(actual.cuts, metric.cuts);
+    });
 
-  it('format function returns correctly formated metric', () => {
-    const metric = metrics.makeHistogram('metric', ['a', 'b'], [0.1, 0.2, 0.5], '');
-    const stats = {
-      a: {
-        b: {
-          sum: 42,
-          count: 12,
-          counts: [
-            5,
-            3,
-            1,
-            3
-          ]
-        }
-      }
-    };
-    const actual = metric.format(stats);
-    const expected = metrics.formatHeader(metric) +
-      '\nmetric_bucket{le="0.1"} 5' +
-      '\nmetric_bucket{le="0.2"} 8' +
-      '\nmetric_bucket{le="0.5"} 9' +
-      '\nmetric_bucket{le="+Inf"} 12' +
-      '\nmetric_count 12' +
-      '\nmetric_sum 42'
-    assert.equal(actual, expected);
-  });
+    it('format function returns correctly formated metric', () => {
+        const metric = metrics.makeHistogram('metric', ['a', 'b'], [0.1, 0.2, 0.5], '');
+        const stats = {
+            a: {
+                b: {
+                    sum: 42,
+                    count: 12,
+                    counts: [
+                        5,
+                        3,
+                        1,
+                        3
+                    ]
+                }
+            }
+        };
+        const actual = metric.format(stats);
+        const expected = metrics.formatHeader(metric) +
+            '\nmetric_bucket{le="0.1"} 5' +
+            '\nmetric_bucket{le="0.2"} 8' +
+            '\nmetric_bucket{le="0.5"} 9' +
+            '\nmetric_bucket{le="+Inf"} 12' +
+            '\nmetric_count 12' +
+            '\nmetric_sum 42'
+        assert.equal(actual, expected);
+    });
 });
 
 describe('formatAll', () => {
-  it('returns the formatted values of all registered metrics', () => {
-    metrics.registerGauge('gauge_value', ['a', 'b'], '')
-    metrics.registerCounter('counter_value', ['a', 'c'], '')
+    it('returns the formatted values of all registered metrics', () => {
+        metrics.registerOneDimensionalGauge('one_dimensional_gauge', ['a', 'b'], '')
+        metrics.registerMultiDimensionalGauge({
+            name: 'multi_dimensional_gauge',
+            help: 'help',
+            dimensions: [
+                {name: "multi_dimensional_gauge{kind=\"a.b\"}", path: ["a", "b"]},
+                {name: "multi_dimensional_gauge{kind=\"a.c\"}", path: ["a", "c"]}
+            ]
+        })
+        metrics.registerCounter('counter_value', ['a', 'c'], '')
 
-    const stats = {a: {b: 42, c: 123}};
+        const stats = {a: {b: 42, c: 123}};
 
-    const expected = 
-      '# HELP gauge_value \n' +
-      '# TYPE gauge_value gauge\n' +
-      'gauge_value 42\n' +
-      '# HELP counter_value \n' +
-      '# TYPE counter_value counter\n' +
-      'counter_value 123\n';
+        const expected =
+            '# HELP one_dimensional_gauge \n' +
+            '# TYPE one_dimensional_gauge gauge\n' +
+            'one_dimensional_gauge 42\n' +
+            '# HELP multi_dimensional_gauge help\n' +
+            '# TYPE multi_dimensional_gauge gauge\n' +
+            'multi_dimensional_gauge{kind="a.b"} 42\n' +
+            'multi_dimensional_gauge{kind="a.c"} 123\n' +
+            '# HELP counter_value \n' +
+            '# TYPE counter_value counter\n' +
+            'counter_value 123\n';
 
-    const actual = metrics.formatAll(stats);
-    assert.equal(actual, expected);
-  });
+        const actual = metrics.formatAll(stats);
+        assert.equal(actual, expected);
+    });
 });
